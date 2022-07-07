cbb52f0030c19b2dac5533834df2b00d59f24a9f
Vladislav <vladmihin28@gmail.com>
2022-07-07 23:34:10 +0300

    обработчик удаления клиентов

---
+++ Handler/Delete/Client.php
@@ -0,0 +1,19 @@
+<?php
+
+include $_SERVER['DOCUMENT_ROOT'] . "/Database/Client.php";
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: DELETE");
+
+$input = file_get_contents("php://input");
+$inputJSON = json_decode( $input, TRUE );
+
+$db = new Client();
+$db->Set($inputJSON["phone_num"]);
+$result = $db->Delete();
+if (!$result) {
+    echo "not found";
+} else {
+    echo "rows delete ".$result;
+}
+
