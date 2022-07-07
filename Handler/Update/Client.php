1512a4a98d54eb118189248cd888d4fa295b5d2b
Vladislav <vladmihin28@gmail.com>
2022-07-07 23:27:56 +0300

    обработчик обновления клиентов

---
+++ Handler/Update/Client.php
@@ -0,0 +1,19 @@
+<?php
+
+include $_SERVER['DOCUMENT_ROOT'] . "/Database/Client.php";
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: PUT");
+
+$input = file_get_contents("php://input");
+$inputJSON = json_decode( $input, TRUE );
+
+$db = new Client();
+$db->Set($inputJSON["phone_num"],$inputJSON["firstname"],$inputJSON["lastname"],$inputJSON["birthday"]);
+$result = $db->Update();
+if (!$result) {
+    echo "not found";
+} else {
+    echo "rows update ".$result;
+}
+
