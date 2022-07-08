a84f3ea1c6d9403a22b9a81c870caa2c1af9624c
Vladislav <vladmihin28@gmail.com>
2022-07-08 09:31:48 +0300

    обработка запросов

--- Database/Order.php
+++ Database/Order.php
@@ -11,7 +11,7 @@ class Order extends ConnDB implements Database
     private string $body;
     private int $status;
 
-    public function Set(string $phone_num = "", string $to = "", string $body = "", int $status = 0)
+    public function Set(string $phone_num = "", string $to = "", string $body = "", int $status = 1)
     {
         $this->phone_num = $phone_num;
         $this->to = $to;
---
+++ Handler/Update/Order.php
@@ -0,0 +1,18 @@
+<?php
+
+include $_SERVER['DOCUMENT_ROOT'] . "/Database/Order.php";
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: PUT");
+
+$input = file_get_contents("php://input");
+$inputJSON = json_decode( $input, TRUE );
+
+$db = new Order();
+$db->Set($inputJSON["phone_num"],$inputJSON["send"],$inputJSON["body"],$inputJSON["status"]);
+$result = $db->Update();
+if (!$result) {
+    echo "not found";
+} else {
+    echo "rows update ".$result;
+}
